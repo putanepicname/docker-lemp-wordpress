a1038aeda3c2121b0769d2ee729874aef4879c13
Umair <engineerhead@gmail.com>
2017-03-04 22:39:22 +0500

    Adding sleep after docker-compose up

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -e;
+set -ex;
 
 html_result=$(curl -sL localhost:8000);
 grep_result=$(grep -o Afrikaans <<< "$html_result");
 if [ $grep_result != "Afrikaans" ]; then
-  echo "$grep_result"
+  echo "$html_result"
   exit 1;
 else
   echo "Wordpress installed";
